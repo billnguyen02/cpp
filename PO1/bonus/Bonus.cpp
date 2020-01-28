6eacf9f6ddc03a8844651c2122235f7c90c07c54
Hung Nguyen <hung.nguyen3@mavs.uta.edu>
2020-01-28 00:18:33 -0600

    Bonus Question

---
+++ PO1/bonus/Bonus.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[]) {
+    string name;
+    cout << "Enter your name" << endl;
+    getline(cin,name);
+    cout << "\nMy name is " << name << endl;
+
+  return 0;
+}
