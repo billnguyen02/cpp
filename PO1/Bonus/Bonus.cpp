f2adb517f2cd4134565d1c0110cff9adcfe2e663
Hung Nguyen <hung.nguyen3@mavs.uta.edu>
2020-01-28 00:47:13 -0600

    Bonus

---
+++ PO1/Bonus/Bonus.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[]) {
+    string name;
+    cout << "Enter your name" << endl;
+    getline(cin,name);
+    cout << "\nHello, " << name << endl;
+
+  return 0;
+}
